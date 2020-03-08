b76f92585c27db09930f172077efd1aa9b67888e
Ashish Kujur <ashish.xenon@gmail.com>
2020-03-08 14:13:49 +0530

    Update Largest-histogram.cpp
    Added Algorithm summary for the code.

--- Stack_Algorithms/Largest-histogram.cpp
+++ Stack_Algorithms/Largest-histogram.cpp
@@ -1,3 +1,21 @@
+/*
+Algorithm to find the largest rectangle in the histogram.
+1.  Insert index values in the stack till you reach an element who's value is smaller
+  than the values of the top element of the corresponding index value in the stack.
+    -> This would mean that the bars from stack[top]th index and the ith index
+       can form a rectangle, by chaining the corresponding bars of the histogram.
+
+       * area = top element x (ith index - stack.top index {-1 because 0-index})
+
+       * area = top element x ith index  // When top element is the smallest bar in the stack(up till that point)
+                                            so it will be multiplied by all the bars which were
+                                            coverd up till that point.
+
+2.  Keep comparing and updating this area with the max area obtained previously.
+
+3. If the stack is still left with indices that means that no smaller height element was found,
+  Keep popping out elements form the stack and repeat the same proceedure.
+*/
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -19,14 +37,16 @@ int main()
             top = height.top();                 // store the stack top (larger) element.
             height.pop();                       // pop that mofo out.
             if(height.empty()) area = arr[top] * i;    // if the stack is exhausted then there are i chain before it.
-            else area = arr[top] * (i - height.top() - 1); // arr[top] ranges from i to the next element in the stack.
+            else area = arr[top] * (i - height.top() - 1); // height.top = element whoes chain is running which came before i(current index) ; -1 because 0-index
             if(area > max) max = area;  // check if the calculated area is maximum.
         }
     }
     while(!height.empty()){            // for all the remaing continuation of heights. (backtracking...sort of.)
         top = height.top();
         height.pop();
-        if(height.empty()) area = arr[top] * i;
+        if(height.empty()) area = arr[top] * i; // at the final stage of the computation i would be equal to n.
+                                                // and arr[top] would have the value of the last element in the stack which would be the smallest value.
+                                                // hence n x smallest value would give the area where all the bars are included.
         else area = arr[top] * (i - height.top() - 1);
         if(area > max) max = area;
     }
